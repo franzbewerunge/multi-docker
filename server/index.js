2da0e14513c59c2f68d661ae445f8743935e7951
Franz Bewerunge <franz.bewerunge@tier.app>
2021-01-16 17:03:56 +0100

    Rebuild from Template

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const pgClient = new Pool({
 pgClient.on('connect', () => {
   pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log("ERROR POSTGRES", err));
+    .catch((err) => console.log(err));
 });
 
 // Redis Client Setup
